e58c4535c0085c200535b87246de58775760d26b
nhiroyasu <nhiro1109pri@gmail.com>
2025-03-13 22:32:48 +0900

    Fix issue where the app does not work on macOS 15

--- ScrollableSimulator/Util/CGEvent+.swift
+++ ScrollableSimulator/Util/CGEvent+.swift
@@ -95,3 +95,96 @@ func getXScrollQuantity(from cfData: CFData) -> Int32 {
     let signedScrollQuantity = data.withUnsafeBytes { $0.load(as: Int32.self) }
     return signedScrollQuantity
 }
+
+@available(macOS 12.0, *)
+func newCGEvent(baseEvent: CGEvent, addPoint: CGPoint) -> CGEvent? {
+    let globalLocationAddress: [UInt8] = [0x00, 0x02, 0xc0, 0x38]
+    let localLocationAddress: [UInt8] = [0x00, 0x02, 0xc0, 0x39]
+
+    let mutableData = CFDataCreateMutableCopy(nil, 0, baseEvent.data)
+    guard let mutableDataRef = CFDataGetMutableBytePtr(mutableData) else { return nil }
+
+    // Update global location
+    guard let globalLocationOffset: Int = CFComputeNextOffset(bytes: mutableDataRef, count: CFDataGetLength(mutableData), target: globalLocationAddress) else {
+        return nil
+    }
+    let globalXBytes: [UInt8] = [
+        mutableDataRef[globalLocationOffset + 0],
+        mutableDataRef[globalLocationOffset + 1],
+        mutableDataRef[globalLocationOffset + 2],
+        mutableDataRef[globalLocationOffset + 3]
+    ]
+    let globalYBytes: [UInt8] = [
+        mutableDataRef[globalLocationOffset + 4],
+        mutableDataRef[globalLocationOffset + 5],
+        mutableDataRef[globalLocationOffset + 6],
+        mutableDataRef[globalLocationOffset + 7]
+    ]
+    let globalX = bytesToFloat(globalXBytes)
+    let globalY = bytesToFloat(globalYBytes)
+    let newGlobalX: Float32 = globalX + Float32(addPoint.x)
+    let newGlobalY: Float32 = globalY + Float32(addPoint.y)
+    let newGlobalXBytes = withUnsafeBytes(of: newGlobalX.bitPattern.bigEndian) { Array($0) }
+    let newGlobalYBytes = withUnsafeBytes(of: newGlobalY.bitPattern.bigEndian) { Array($0) }
+    mutableDataRef[globalLocationOffset + 0] = newGlobalXBytes[0]
+    mutableDataRef[globalLocationOffset + 1] = newGlobalXBytes[1]
+    mutableDataRef[globalLocationOffset + 2] = newGlobalXBytes[2]
+    mutableDataRef[globalLocationOffset + 3] = newGlobalXBytes[3]
+    mutableDataRef[globalLocationOffset + 4] = newGlobalYBytes[0]
+    mutableDataRef[globalLocationOffset + 5] = newGlobalYBytes[1]
+    mutableDataRef[globalLocationOffset + 6] = newGlobalYBytes[2]
+    mutableDataRef[globalLocationOffset + 7] = newGlobalYBytes[3]
+
+    // Update local location
+    guard let localLocationOffset: Int = CFComputeNextOffset(bytes: mutableDataRef, count: CFDataGetLength(mutableData), target: localLocationAddress) else {
+        return nil
+    }
+    let localXBytes: [UInt8] = [
+        mutableDataRef[localLocationOffset + 0],
+        mutableDataRef[localLocationOffset + 1],
+        mutableDataRef[localLocationOffset + 2],
+        mutableDataRef[localLocationOffset + 3]
+    ]
+    let localYBytes: [UInt8] = [
+        mutableDataRef[localLocationOffset + 4],
+        mutableDataRef[localLocationOffset + 5],
+        mutableDataRef[localLocationOffset + 6],
+        mutableDataRef[localLocationOffset + 7]
+    ]
+    let localX = bytesToFloat(localXBytes)
+    let localY = bytesToFloat(localYBytes)
+    let newLocalX: Float32 = localX + Float32(addPoint.x)
+    let newLocalY: Float32 = localY + Float32(addPoint.y)
+    let newLocalXBytes = withUnsafeBytes(of: newLocalX.bitPattern.bigEndian) { Array($0) }
+    let newLocalYBytes = withUnsafeBytes(of: newLocalY.bitPattern.bigEndian) { Array($0) }
+    mutableDataRef[localLocationOffset + 0] = newLocalXBytes[0]
+    mutableDataRef[localLocationOffset + 1] = newLocalXBytes[1]
+    mutableDataRef[localLocationOffset + 2] = newLocalXBytes[2]
+    mutableDataRef[localLocationOffset + 3] = newLocalXBytes[3]
+    mutableDataRef[localLocationOffset + 4] = newLocalYBytes[0]
+    mutableDataRef[localLocationOffset + 5] = newLocalYBytes[1]
+    mutableDataRef[localLocationOffset + 6] = newLocalYBytes[2]
+    mutableDataRef[localLocationOffset + 7] = newLocalYBytes[3]
+
+    #if DEBUG
+    print("globalX: \(globalX), globalY: \(globalY), localX: \(localX), localY: \(localY)")
+    print("newGlobalX: \(newGlobalX), newGlobalY: \(newGlobalY), newLocalX: \(newLocalX), newLocalY: \(newLocalY)")
+    print("---")
+    #endif
+
+    return CGEvent(withDataAllocator: kCFAllocatorDefault, data: mutableData)
+}
+
+private func CFComputeNextOffset(bytes: UnsafeMutablePointer<UInt8>, count: Int, target: [UInt8]) -> Int? {
+    for i in 0..<count {
+        for targetIndex in 0..<target.count {
+            if bytes[i + targetIndex] != target[targetIndex] {
+                break
+            }
+            if targetIndex == target.count - 1 {
+                return i + target.count
+            }
+        }
+    }
+    return nil
+}
